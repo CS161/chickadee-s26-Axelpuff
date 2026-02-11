fba987e41c4985ecf243d8cf82c137f787352b70
Axel Soderquist <asoderquist@college.harvard.edu>
2026-02-11 12:42:27 -0500

    Added a validation function that seems to work

--- k-alloc.cc
+++ k-alloc.cc
@@ -46,6 +46,39 @@ struct free_list_entry {
 
 list<free_list_entry, &free_list_entry::link_> free_lists[max_order + 1];
 
+
+
+
+void validate_free(uintptr_t ptr) {
+    assert((ptr & PAGEOFFMASK) == 0);
+    page_entry header = pages[ptr / PAGESIZE];
+    assert(header.allocatable);
+    assert(header.free);
+    assert(header.order >= min_order && header.order <= max_order);
+    size_t block_sz_bytes = 1u << header.order;
+    size_t block_sz_pages = block_sz_bytes / PAGESIZE;
+    for (size_t pg_offset = 1; pg_offset < block_sz_pages; pg_offset++) {
+        page_entry entry = pages[(ptr / PAGESIZE) + pg_offset];
+        assert(entry.allocatable);
+        assert(entry.free);
+        assert(entry.order == -1);
+    }
+}
+
+void validate_free_lists() {
+    log_printf("Validating `free_lists`\n");
+    for (int order = min_order; order <= max_order; order++) {
+        for (free_list_entry* list_entry = free_lists[order].front();
+             list_entry != nullptr;
+             list_entry = free_lists[order].next(list_entry)) {
+            if (list_entry) {
+                log_printf("Found free list with entry of value %zu\n", list_entry->block_start_);
+                validate_free(list_entry->block_start_);
+            }
+        }
+    }
+}
+
 void mark_range(const memrange* range, bool allocatable) {
     for (uintptr_t i = range->first(); i < range->last(); i += PAGESIZE) {
         assert((i & PAGEOFFMASK) == 0);
@@ -69,20 +102,25 @@ void make_order_blocks(const memrange* range) {
         int space_limited_order = msb(range->last() - aligned_addr) - 1;
         int addr_limited_order = msb(aligned_addr) - 1;
         int order = min(space_limited_order, addr_limited_order);
+        order = min(order, max_order);
         // log_printf("space order %u\n", space_limited_order);
         // log_printf("addr order %u\n", addr_limited_order);
         log_printf("found existent range of order %u\n", order);
-        // log_printf("starting at: %p\n", aligned_addr);
-        // log_printf("dividing by: %p\n", 1u << order);
-        assert(aligned_addr % (1u << order) == 0);
-        log_printf("found existent range of size %zu pages\n", range->size() / PAGESIZE);
-        assert((aligned_addr & PAGEOFFMASK) == 0);
-        // Update pages array
-        pages[aligned_addr / PAGESIZE].order = order;
-        // Update free lists
-        log_printf("Block starts at: %zu\n", aligned_addr);
-        free_list_entry* entry = knew<free_list_entry>(aligned_addr);
-        free_lists[order].push_front(entry);
+        if (order >= min_order) {
+            // log_printf("starting at: %p\n", aligned_addr);
+            // log_printf("dividing by: %p\n", 1u << order);
+            assert(aligned_addr % (1u << order) == 0);
+            log_printf("found existent range of size %zu pages\n", range->size() / PAGESIZE);
+            assert((aligned_addr & PAGEOFFMASK) == 0);
+            // Update pages array
+            pages[aligned_addr / PAGESIZE].order = order;
+            // Update free lists
+            log_printf("Block starts at: %zu\n", aligned_addr);
+            free_list_entry* entry = knew<free_list_entry>(aligned_addr);
+            free_lists[order].push_front(entry);
+        } else {
+            log_printf("Order too small\n");
+        }
     }
 }
 
@@ -119,12 +157,10 @@ void init_kalloc() {
             log_printf("Found page of order %u\n", pages[pg].order);
         }
     }
-    for (int order = min_order; order <= max_order; order++) {
-        free_list_entry* front = free_lists[order].front();
-        if (front) {
-            log_printf("Found free list with first entry of value %zu\n", front->block_start_); 
-        }
-    }
+    
+    // free_list_entry* bad_entry = knew<free_list_entry>(0x1000);    
+    // free_lists[13].push_front(bad_entry);
+    validate_free_lists();
     assert(max_order >= min_order);
     // !!! I can't get a sufficiently large order without modifying k-init to get even more starting memory
     assert(max_order >= 21);
@@ -160,6 +196,29 @@ void* kalloc(size_t sz) {
     auto irqs = page_lock.lock();
     void* ptr = nullptr;
 
+    int target_order = max(msb(sz) - 1, min_order); // require min_order
+    // iterate over free_lists from target_order to max_order (inclusive)
+    //  if we find a non-empty free list, set ptr to the first list entry and break
+    // if ptr is still null, return ptr (null)
+    // if order is greater than target, split buddies!!!
+    // SPLIT:
+    //  {
+    //  deincrement this buddy's order
+    //  find the midway point, assert allocatable, free, and order == -1, change order to this buddy's order
+    //   (to be super paranoid: check that every page within this buddy apart from the first is allocatable, free, and order == -1)
+    //  remove ptr from list; add ptr and midway point to list of order below
+    //  }
+    //  deincrement order, rinse and repeat until order == target (should always work)
+    // TAKE:
+    // (if order is equal to target,) mark all pages within buddy as non-free, remove ptr from free list, then return ptr
+    // increment allocated_pages
+
+    // helper function void split_buddy(addr) (uses pages[addr / PAGESIZE] metadata)
+    // helper function void take_buddy(addr)
+
+    // basically we find, split as needed, and then take
+    
+    // OLD!!!!
     // skip over reserved and kernel memory
     auto range = physical_ranges.find(next_free_pa);
     while (range != physical_ranges.end()) {
@@ -188,7 +247,6 @@ void* kalloc(size_t sz) {
     return ptr;
 }
 
-
 // kfree(ptr)
 //    Free a pointer previously returned by `kalloc`. Does nothing if
 //    `ptr == nullptr`.
@@ -198,6 +256,28 @@ void kfree(void* ptr) {
         asan_mark_memory(ka2pa(ptr), PAGESIZE, true);
         // remember to deincrement allocated pages
     }
+    // In this case the situation is reversed. We give, and then merge as needed.
+    // Give:
+    //  (pages) mark all pages in buddy as free, (list) add it to the appropriate free list
+    //  deincrement allocated_pages
+    // Merge(ptr): functions the same way whether you pick the first or second buddy.
+    //  (pages) find root buddy; this involves dividing and re-multiplying ptr by 2^(next order) (make sure to convert to pages)
+    //   then find next buddy by adding 2^(order)
+    //   check whether both buddy headers are free
+    //    (paranoia: at least one of the buddies should be free, as in its header is allocatable, free, and order != -1, and the other should be either free or correctly allocated: alloctable, all pages non-free, and order != -1 only on the header)
+    //   one isn't free: return nullptr
+    //   both are free:
+    //    (pages) set beginning header to (next order) and midpoint header to order -1
+    //    (list) remove list entries for both buddies; add one entry to the (next order) list
+    //    (paranoia: free check on new merged block)
+    //    (can also have an (externally callable?) global check to see if the data structures exactly match)
+    //    return the start address of the new merged block
+    // Rinse and repeat merging
+    
+    // helper functions:
+    // validate_free(ptr): Validates that the layout of (pages) starting at ptr is correct for a free allocation (uses pages[ptr / PAGESIZE].order to determine order)
+    // validate_allocated(ptr): Ditto, for allocated
+    // validate_all_free(ptr): Traverses free_lists and calls validate_free on all pointers it finds
     log_printf("kfree not implemented yet\n");
 }
 
